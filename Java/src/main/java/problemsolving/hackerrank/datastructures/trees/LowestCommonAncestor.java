cb5fc419ccc66e8883bd8bf5ccc116bc68237831
Ganesh Satpute <ghsatpute@gmail.com>
2020-03-02 18:24:40 +0530

    Implemented lowest common ancestor problem

---
+++ Java/src/main/java/problemsolving/hackerrank/datastructures/trees/LowestCommonAncestor.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static Node lca(Node root, int v1, int v2) {
+      	if (v1 > v2) {
+              int temp = v1; 
+              v1 = v2; 
+              v2 = temp; 
+        }
+
+        Node current = root; 
+
+        while (current != null) {
+            if (v1 <= current.data && v2 >= current.data) {
+                return current; 
+            }
+            if (v1 < current.data && v2 < current.data) {
+                current = current.left; 
+            }
+            if (v1 > current.data && v2 > current.data) {
+                current = current.right; 
+            }
+        }
+        return null; 
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+      	int v1 = scan.nextInt();
+      	int v2 = scan.nextInt();
+        scan.close();
+        Node ans = lca(root,v1,v2);
+        System.out.println(ans.data);
+    }	
+}
