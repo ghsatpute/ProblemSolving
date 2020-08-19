629874b8124cd63a64e12aa229b150344758372b
Satpute, Ganesh <Ganesh.Satpute@Teradata.com>
2020-08-19 22:53:32 +0530

    Solved search in rotated sorted array

---
+++ Java/src/main/java/problemsolving/leetcode/datastructures/arrays/searchInRotatedSortedArray/SearchInRotatedSortedArray.java
@@ -0,0 +1,38 @@
+package problemsolving.leetcode.datastructures.arrays.searchInRotatedSortedArray;
+
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int pivot = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
+        if (nums[0] <= target && target <= nums[pivot]) {
+            return binarySearch(nums, 0, pivot, target);
+        } else {
+            return binarySearch(nums, pivot + 1, nums.length - 1, target);
+        }
+    }
+
+    private int binarySearch(int[] array, int start, int end, int target) {
+        if (end < start) {
+            return -1;
+        }
+        if (end == start) {
+            if (array[start] == target) {
+                return start;
+            } else {
+                return -1;
+            }
+        }
+        int mid = (start + end) / 2;
+        System.out.println("Mid is " + mid);
+        if (target <= array[mid]) {
+            return binarySearch(array, start, mid, target);
+        } else {
+            return binarySearch(array, mid + 1, end, target);
+        }
+    }
+}
