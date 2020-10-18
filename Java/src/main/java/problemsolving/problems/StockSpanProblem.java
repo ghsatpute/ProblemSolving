8959e6973af8f6b526508b1537d888bc06a4cfff
Satpute, Ganesh <Ganesh.Satpute@Teradata.com>
2020-10-18 15:58:49 +0530

    Solved Stock Span Problem

---
+++ Java/src/main/java/problemsolving/problems/StockSpanProblem.java
@@ -0,0 +1,30 @@
+package problemsolving.problems;
+
+import java.util.Stack;
+
+public class StockSpanProblem {
+    public static int[] solve(int[] stockPrices) {
+        if (stockPrices.length == 0) {
+            return new int[]{};
+        }
+
+        if (stockPrices.length == 1) {
+            return new int[] {1};
+        }
+        int[] output = new int[stockPrices.length];
+        output[0] = 1;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int i = 1; i < stockPrices.length; i++) {
+            while (!stack.empty() && stockPrices[stack.peek()] < stockPrices[i]) {
+                stack.pop();
+            }
+
+            output[i] = i - stack.peek();
+            stack.push(i);
+        }
+        return output;
+    }
+}
---
+++ Java/src/test/java/problemsolving/problems/StockSpanProblemTest.java
@@ -0,0 +1,16 @@
+package problemsolving.problems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StockSpanProblemTest {
+    @Test
+    public void testCase01() {
+        int[] input = new int[] {100, 80, 60, 70, 60, 75, 85};
+
+        int[] output = StockSpanProblem.solve(input);
+
+        assertArrayEquals(new int[] {1, 1, 1, 2, 1, 4, 6}, output);
+    }
+}
